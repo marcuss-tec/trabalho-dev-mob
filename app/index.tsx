779c20b7716ad098ec008ad5e45cad4478537955
marcuss <diasjoaomarcus@gmail.com>
2026-05-20 23:27:04 -0300

    projeto atualizado

--- app/index.tsx
+++ app/index.tsx
@@ -2,41 +2,45 @@ import { Link } from 'expo-router';
 //import { KeyRound, Mail } from 'lucide-react-native';
 import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
-export default function app() {
+
+export default function HomeScreen() {
   return (
     <View style={styles.containerBody}>
-      <View style={styles.containerLogo}>
-        {/*<Image
-          source={require('../../assets/images/logo.png')}
-        />*/}
-        <Text style={styles.textBemVindo}>Bem vindo de Volta!</Text>
+      <View style={styles.containerLogo}>''
+        <Image
+          source={require('../assets/images/gatao.jpg')}
+        />
+        <Text style={styles.easyList}>EasyList</Text>
+      <View style={styles.textAcimaForm}>
+        <Text style={styles.suaLista}>"Sua lista de compras, mais fácil"</Text>
+      </View>  
       </View>
-      <View style={styles.viewForm}>
-        <View>
-            <Text style={styles.textForm}>Email</Text>
-            <View style={styles.iconBlock}>
-              <TextInput
-                style={styles.input}
-                placeholder=''
-              />
-              {/*<Mail style={styles.icone} />*/}
-            </View>
-        </View>
-        <View>
+        <View style={styles.viewForm}>
           <View>
-            <Text style={styles.textForm}>Senha</Text>
-            <View style={styles.iconBlock}>
-              <TextInput
-                style={styles.input}
-                placeholder=''
-                secureTextEntry={true}
-                textContentType='password'
-              />
-              {/*<KeyRound style={styles.icone} />*/}
+              <Text style={styles.textForm}>Email</Text>
+              <View style={styles.iconBlock}>
+                <TextInput
+                  style={styles.input}
+                  placeholder=''
+                />
+                {/*<Mail style={styles.icone} />*/}
+              </View>
+          </View>
+          <View>
+            <View>
+              <Text style={styles.textForm}>Senha</Text>
+              <View style={styles.iconBlock}>
+                <TextInput
+                  style={styles.input}
+                  placeholder=''
+                  secureTextEntry={true}
+                  textContentType='password'
+                />
+                {/*<KeyRound style={styles.icone} />*/}
+              </View>
             </View>
           </View>
         </View>
-      </View>
       <View style={styles.viewBtn}>
         <View>
           <TouchableOpacity style={styles.btnEnviar}>
@@ -56,26 +60,36 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff"
   },
   containerLogo: {
-    flex: 1,
+    flex: 0.6,
     alignItems: 'center',
     justifyContent: 'center'
   },
   input: {
+    margin:20,
     backgroundColor: '#d7d8d7',
-    borderRadius: 20
+    borderRadius: 40,
+    paddingVertical: 14,
+    marginLeft:15,
+    paddingHorizontal:25,
   },
-  textBemVindo: {
+  easyList: {
     fontSize: 30,
     fontWeight: 'bold',
      paddingTop: 20
   },
   viewForm: {
-    flex: 1,
-    paddingHorizontal: 15
+    backgroundColor: '#111377',
+    flex: 0.4,
+    paddingHorizontal: -30,
+    borderRadius:25,
+    marginHorizontal:10,
   },
   textForm: {
-    fontSize: 19,
-    color: '#d7d8d7'
+    opacity:0.5,
+    fontSize: 25,
+    color: '#d7d8d7',
+    paddingTop:15,
+    paddingLeft:18,
   },
   icone: {
     position: 'absolute',
@@ -93,13 +107,21 @@ const styles = StyleSheet.create({
   btnEnviar:{
     backgroundColor: '#878af6',
     paddingVertical: 10,
-    paddingHorizontal: 70,
+    paddingHorizontal: 50,
     borderRadius: 35
 },
   viewBtn:{
-    flex: 1, 
-    marginTop: -250,
+    flex:0.2,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor:'#ffffff'
+  },
+
+  suaLista:{
+    flex:0,
+  },
+
+  textAcimaForm:{
+    flex:0,
   }
 })
